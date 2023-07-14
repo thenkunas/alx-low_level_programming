1eb544bd0ebd692b30d394022f14bcb78f9b067c
thenkunas <nkunasjunior@gmail.com>
2023-07-14 08:58:36 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,9 @@
 #include<stdio.h>
-
+/**
+* main - point of entry
+*
+* Return: returns 0
+*/
 int main(void)
 {
 	printf("Size of a char: %d byte(s)\n", sizeof(char));
@@ -9,3 +13,4 @@ int main(void)
 	printf("Size of a float: %d byte(s)\n", sizeof(float));
 	return (0);
 }
+
