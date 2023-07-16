40b8b15de3d7627f86ba85e63aa26ae12211a5db
thenkunas <nkunasjunior@gmail.com>
2023-07-16 08:06:04 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Prints diogit 0-9
+ *
+ * Return: 0 
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	putchar('\n');
+	return (0);
+}
