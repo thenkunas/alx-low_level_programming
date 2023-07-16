d700617468666a8a9e0095023c106244f71b3ec2
thenkunas <nkunasjunior@gmail.com>
2023-07-16 08:08:03 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Printsdigits 0-9.
+ *
+ * Return:  0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 48; i < 58; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
