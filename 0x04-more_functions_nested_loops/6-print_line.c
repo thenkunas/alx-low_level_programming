f7478c6c368b12c9cc2bb5b39b1eb2233c6b8a29
thenkunas <nkunasjunior@gmail.com>
2023-07-20 20:25:44 -0700

    6-print_line.c

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * print_line - prints staright line
+ * @n: how many times character is prnmted
+ */
+void print_line(int n)
+{
+	if (n <= 0)
+	{
+		_putchar('\n');
+	} else
+	{
+		int i;
+
+		for (i = 1; i <= n; i++)
+		{
+			_putchar('_');
+		}
+		_putchar('\n');
+	}
+
+}
